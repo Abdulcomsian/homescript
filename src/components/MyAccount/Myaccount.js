9cb9b7cb0f5a61f5097ed6eba5f06d74e977b00d
RanaNouman888 <nouman.arshad@accrualhub.com>
2024-01-31 16:40:02 +0500

    new changes

--- src/components/MyAccount/Myaccount.js
+++ src/components/MyAccount/Myaccount.js
@@ -4,27 +4,32 @@ function Myaccount() {
       <div className="Myaccount">
         <div className="Details-setion">
           <div className="card">
-            <div class="header">
-              <h3>Details</h3>
-            </div>
             <div class="card-body ">
               <form>
-                <div className="row formSection">
-                  <div className="col-sm-6 mb-3">
-                    <label>Name</label>
-                    <input type="text" class="form-control" />
-                  </div>
-                  <div className="col-sm-6 mb-3">
-                    <label>Email</label>
-                    <input type="email" class="form-control" />
-                  </div>
-                  <div className="col-sm-6 mb-3">
-                    <label>Role</label>
-                    <input type="text" class="form-control" />
-                  </div>
-                  <div className="col-sm-6 mb-3">
-                    <label>Website</label>
-                    <input type="text" class="form-control" />
+                <div className=" ">
+                  <div className="main">
+                    <div class="header">
+                      <h3>Details</h3>
+                    </div>
+                    <div></div>
+                    <div className="row formSection">
+                      <div className="col-sm-6 mb-3">
+                        <label>Name</label>
+                        <input type="text" class="form-control" />
+                      </div>
+                      <div className="col-sm-6 mb-3">
+                        <label>Email</label>
+                        <input type="email" class="form-control" />
+                      </div>
+                      <div className="col-sm-6 mb-3">
+                        <label>Role</label>
+                        <input type="text" class="form-control" />
+                      </div>
+                      <div className="col-sm-6 mb-3">
+                        <label>Website</label>
+                        <input type="text" class="form-control" />
+                      </div>
+                    </div>
                   </div>
                 </div>
                 <div className="detailsBtn-section">
@@ -42,13 +47,14 @@ function Myaccount() {
 
         <div className="Password-section">
           <div className="card">
-            <div class="header">
-              <h3>Change password</h3>
-            </div>
             <div class="card-body ">
               <form>
                 <div className="formSection">
                   <div className="main">
+                    <div class="header">
+                      <h3>Change password</h3>
+                    </div>
+                    <div></div>
                     <div>
                       <div className="mb-3">
                         <label>Current password</label>
@@ -66,6 +72,14 @@ function Myaccount() {
                     <div className="Password-Details">
                       <div className="title">
                         <h3>Password must contain :</h3>
+                      </div>
+                      <div className="List">
+                        <ul>
+                          <li>At least 6 character</li>
+                          <li>At least 1 uppercase letter(A..Z)</li>
+                          <li>At least 1 lowercase letter(a..z)</li>
+                          <li>At least 1 number(0..9)</li>
+                        </ul>
                       </div>
                     </div>
                   </div>
