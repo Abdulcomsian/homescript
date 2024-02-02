7949cf39d03f85d894ddec3b614f9ac6324f55e0
RanaNouman888 <nouman.arshad@accrualhub.com>
2024-02-02 12:01:20 +0500

    sda

--- src/App.js
+++ src/App.js
@@ -1,29 +1,37 @@
 import "./App.css";
 import "./assets/_main.scss";
 import Login from "./components/authentication/login/login";
-import "./assets/_main.scss";
 
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Signup from "./components/authentication/SignUp/Signup";
-import Forgetpassword from "./components/authentication/Forget/forgetpassword";
-import Checkmail from "./components/authentication/checkmail/Checkmail";
-import Resetpassword from "./components/authentication/Reset/Resetpassword";
-import Verification from "./components/authentication/verification/Verification";
-import Sidebar from "./components/SideBar/Sidebar";
-import Topbar from "./components/Topbar/Topbar";
+
 import Dashboard from "./components/Dashboard/Dashboard";
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
+import PrivacyPolicy from "./components/PrivacyPolicy/PrivacyPolicy";
+import TermsAndCondition from "./components/TermAndCondition/TermsAndConditions";
+import Forgetpassword from "./components/authentication/Forget/forgetpassword";
+import Resetpassword from "./components/authentication/Reset/Resetpassword";
+import Signup from "./components/authentication/SignUp/Signup";
+import Checkmail from "./components/authentication/checkmail/Checkmail";
+import Verification from "./components/authentication/verification/Verification";
+import ContactUs from "./components/ContactUs/ContactUs";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route index element={<Login />} />
+        <Route path="/" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/forgot" element={<Forgetpassword />} />
         <Route path="/mail" element={<Checkmail />} />
         <Route path="/reset" element={<Resetpassword />} />
         <Route path="/Verification" element={<Verification />} />
         <Route path="/dashboard/*" element={<Dashboard />} />
+        <Route path="/footer" element={<Footer />} />
+        <Route path="/header" element={<Header />} />
+        <Route path="/termscondition" element={<TermsAndCondition />} />
+        <Route path="/privacypolicy" element={<PrivacyPolicy />} />
+        <Route path="/contactus" element={<ContactUs />} />
       </Routes>
     </BrowserRouter>
   );
